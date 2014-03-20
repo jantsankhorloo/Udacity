8a75a2fdde27922cf2e3ea03c7c8479ea1a5dbeb
jantsankhorloo <jantsankhorloo@gmail.com>
2014-03-20 11:21:34 +0800

    Create L2Ps5.py

---
+++ L2Ps5.py
@@ -0,0 +1,8 @@
+# Define a procedure, stamps, which takes as its input a positive integer in
+# pence and returns the number of 5p, 2p and 1p stamps (p is pence) required 
+# to make up that value. The return value should be a tuple of three numbers 
+# (that is, your return statement should be followed by the number of 5p,
+# the number of 2p, and the nuber of 1p stamps).
+
+def stamps(n):
+    return (n/5, n%5/2, n%5%2) #be careful on how % works
