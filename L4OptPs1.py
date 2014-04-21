e54821ce49c9652326c0e407c8c7e57a09e9cbc5
jantsankhorloo <jantsankhorloo@gmail.com>
2014-04-21 16:51:47 +0800

    Create L4OptPs1.py

---
+++ L4OptPs1.py
@@ -0,0 +1,6 @@
+# Write a procedure, count_words, which takes as input a string
+# and returns the number of words in the string. You may consider words
+# as strings of characters separated by spaces.
+
+def count_words(passage):
+    return len(passage.split())
